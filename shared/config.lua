99c624b11630a51f44fbefa9f5307a701752ed76
KrazyyDev <newkrazyy@proton.me>
2024-05-07 04:32:23 +0200

    add locales

--- shared/config.lua
+++ shared/config.lua
@@ -10,16 +10,62 @@ Config.CommandOpenPedBuilderName = "pedbuilder"
 
 
 --[[
-    "license:56afb46bfc02e66cd9c48e3cb3538df83a8fe844", -- license
-    "steam:11000010b1b1b1b", -- steam
-    "discord:1121068653710938222", -- discord
-    "ip:192.122.122.122", -- ip
-    "live:11000010b1b1b1b", -- liveid
-    "xbl:11000010b1b1b1b" -- xblid
-    "license2:56afb46bfc02e66cd9c48e3cb3538df83a8fe844", -- license
+"license:56afb46bfc02e66cd9c48e3cb3538df83a8fe844", -- license
+"steam:11000010b1b1b1b", -- steam
+"discord:1121068653710938222", -- discord
+"ip:192.122.122.122", -- ip
+"live:11000010b1b1b1b", -- liveid
+"xbl:11000010b1b1b1b" -- xblid
+"license2:56afb46bfc02e66cd9c48e3cb3538df83a8fe844", -- license
 ]]
 
 Config.OpenAuthorisation = {
     "discord:1121068653710938222",
     "license:56afb46bfc02e66cd9c48e3cb3538df83a8fe844"
 }
+
+-- next update
+-- Config.AnimationsListes = {
+--     clipBoard = {animation = 'WORLD_HUMAN_CLIPBOARD', dict = 'WORLD_HUMAN'},
+--     binoculars = {animation = 'WORLD_HUMAN_BINOCULARS', dict = 'WORLD_HUMAN'},
+--     camera = {animation = 'WORLD_HUMAN_PAPARAZZI', dict = 'WORLD_HUMAN'},
+--     coffee = {animation = 'WORLD_HUMAN_AA_COFFEE', dict = 'WORLD_HUMAN'},
+--     smoking = {animation = 'WORLD_HUMAN_SMOKING', dict = 'WORLD_HUMAN'},
+--     drinkFacility = {animation = 'WORLD_HUMAN_DRINKING_FACILITY', dict = 'WORLD_HUMAN'},
+--     golfPlayer = {animation = 'WORLD_HUMAN_GOLF_PLAYER', dict = 'WORLD_HUMAN'},
+--     guardPatrol = {animation = 'WORLD_HUMAN_GUARD_PATROL', dict = 'WORLD_HUMAN'},
+--     lean = {animation = 'WORLD_HUMAN_LEANING', dict = 'WORLD_HUMAN'},
+--     maidClean = {animation = 'WORLD_HUMAN_MAID_CLEAN', dict = 'WORLD_HUMAN'},
+--     muscleFlex = {animation = 'WORLD_HUMAN_MUSCLE_FLEX', dict = 'WORLD_HUMAN'},
+--     yoga = {animation = 'WORLD_HUMAN_YOGA', dict = 'WORLD_HUMAN'},
+--     catSleepGround = {animation = 'WORLD_CAT_SLEEPING_GROUND', dict = 'WORLD_HUMAN'},
+--     dogBarkingRottweiler = {animation = 'WORLD_DOG_BARKING_ROTTWEILER', dict = 'WORLD_HUMAN'},
+--     fishIdle = {animation = 'WORLD_FISH_IDLE', dict = 'WORLD_HUMAN'},
+--     mountainLionRest = {animation = 'WORLD_MOUNTAIN_LION_REST', dict = 'WORLD_HUMAN'},
+--     propHumanBBQ = {animation = 'PROP_HUMAN_BBQ', dict = 'WORLD_HUMAN'},
+--     propHumanMuscleChinUps = {animation = 'PROP_HUMAN_MUSCLE_CHIN_UPS', dict = 'WORLD_HUMAN'},
+--     codeHumanCrossRoadWait = {animation = 'CODE_HUMAN_CROSS_ROAD_WAIT', dict = 'WORLD_HUMAN'},
+--     chainingEntry = {animation = 'CHAINING_ENTRY', dict = 'WORLD_HUMAN'},
+--     earToText = {animation = 'EAR_TO_TEXT', dict = 'WORLD_HUMAN'},
+--     propBirdTelegraphPole = {animation = 'PROP_BIRD_TELEGRAPH_POLE', dict = 'WORLD_HUMAN'},
+--     propHumanSeatBenchFoodFacility = {animation = 'PROP_HUMAN_SEAT_BENCH_FOOD_FACILITY', dict = 'WORLD_HUMAN'},
+--     codeHumanStandCower = {animation = 'CODE_HUMAN_STAND_COWER', dict = 'WORLD_HUMAN'},
+--     worldLookAtPoint = {animation = 'WORLD_LOOKAT_POINT', dict = 'WORLD_HUMAN'},
+--     jogging = {animation = 'WORLD_HUMAN_JOG', dict = 'WORLD_HUMAN'},
+--     cheering = {animation = 'WORLD_HUMAN_CHEERING', dict = 'WORLD_HUMAN'},
+--     sunbathing = {animation = 'WORLD_HUMAN_SUNBATHE', dict = 'WORLD_HUMAN'},
+--     bartender = {animation = 'WORLD_HUMAN_BARTENDER', dict = 'WORLD_HUMAN'},
+--     fishing = {animation = 'WORLD_HUMAN_STAND_FISHING', dict = 'WORLD_HUMAN'},
+--     mechanic = {animation = 'WORLD_HUMAN_VEHICLE_MECHANIC', dict = 'WORLD_HUMAN'},
+--     touristMobile = {animation = 'WORLD_HUMAN_TOURIST_MOBILE', dict = 'WORLD_HUMAN'},
+--     basketballPlayer = {animation = 'WORLD_HUMAN_BASKETBALL_PLAYER', dict = 'WORLD_HUMAN'},
+--     powerWalker = {animation = 'WORLD_HUMAN_POWER_WALKER', dict = 'WORLD_HUMAN'},
+--     gardenerLeafBlower = {animation = 'WORLD_HUMAN_GARDENER_LEAF_BLOWER', dict = 'WORLD_HUMAN'},
+--     windowShopBrowse = {animation = 'WORLD_HUMAN_WINDOW_SHOP_BROWSE', dict = 'WORLD_HUMAN'},
+--     hangOutStreet = {animation = 'WORLD_HUMAN_HANG_OUT_STREET', dict = 'WORLD_HUMAN'},
+--     sitUps = {animation = 'WORLD_HUMAN_SIT_UPS', dict = 'WORLD_HUMAN'},
+--     standMobile = {animation = 'WORLD_HUMAN_STAND_MOBILE', dict = 'WORLD_HUMAN'},
+--     standImpatient = {animation = 'WORLD_HUMAN_STAND_IMPATIENT', dict = 'WORLD_HUMAN'},
+--     superhero = {animation = 'WORLD_HUMAN_SUPERHERO', dict = 'WORLD_HUMAN'}
+-- }
+
